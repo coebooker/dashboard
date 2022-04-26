ae5b993a0818fb1e7a58c64332dca5126f1e0b4b
Coe <coebooker@gmail.com>
2022-04-26 15:24:52 -0500

    It's way too slow when plotting all of april, not sure how to fix that. But It works when plotting just 50 which is what it's doing here

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ UberData = pd.read_csv('uber-trip-data/uber-raw-data-apr14.csv')
 UberData["Date/Time"] = pd.to_datetime(UberData["Date/Time"])
 
 
-def map_func():
+def map_func(df):
     print(open("mapbox_token.txt").read())
+    df = df.head(50)
     px.set_mapbox_access_token(open("mapbox_token.txt").read())
-    df = data.head
     fig = px.scatter_mapbox(df, lat="Lat", lon="Lon", color="Date/Time",
                             color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
     fig.update_layout(title="Map Output")
@@ -98,12 +98,10 @@ def make_plot(date):
     return make_timeplot(UberData, datetime.datetime(year=2014, month=4, day=temp_day.day))
 
 
-@app.callback(Output('map-fig', 'children'), [Input('score', 'value')])
-def make_map():
-    return map_func()
+@app.callback(Output('map-fig', 'figure'), [Input('date-pick', 'useless')])
+def make_map(useless):
+    return map_func(UberData)
 
-def make_map(mapIn):
-    return map_func()
 
 
 # -------------------------- MAIN ---------------------------- #
